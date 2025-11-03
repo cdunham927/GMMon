5f04232c0e657032fb59a2699846f37878e137b4
cdunham927 <cdunham927@gmail.com>
2025-11-03 03:31:00 -0800

    More work on game 10

--- Assets/Scripts/Minigames/MicrocosmicDeductionController.cs
+++ Assets/Scripts/Minigames/MicrocosmicDeductionController.cs
@@ -7,17 +7,16 @@ using UnityEngine.UI;
 public class MicrocosmicDeductionController : MonoBehaviour
 {
     public Sprite[] images;
-    public Image zoomedImage;
+    public SpriteRenderer zoomedImage;
 
-    public TMP_Text commandText;
-    public TMP_Text yurpusCommandText;
+    bool zoomout = false;
+    public float zoomoutTime = 0.1f;
+    public Camera cam;
 
-
-    public float roundTimeSmall = 2f;
-    public float roundTimeBig = 10f;
-    float curTime;
-    public TMP_Text timeRemaining;
-    public TMP_Text timeRemaining2;
+    //private void Awake()
+    //{
+    //    cam = Camera.main;
+    //}
 
     public void StartRound()
     {
@@ -27,23 +26,14 @@ public class MicrocosmicDeductionController : MonoBehaviour
 
     private void Update()
     {
-        //if (curTime > 0) curTime -= Time.deltaTime;
-
-        //Show time remaining
-        //int seconds = ((int)curTime % 60);
-        //int minutes = ((int)curTime / 60);
-        //timeRemaining.text = string.Format("{0:00}:{1:00}", minutes, seconds);
-        //timeRemaining2.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+        if (zoomout) cam.orthographicSize += zoomoutTime * Time.deltaTime;
     }
 
     public void GetInstruction()
     {
+        zoomout = true;
+
         int index = Random.Range(0, images.Length);
         zoomedImage.sprite = images[index];
-
-        //commandText.text = currentObject;
-        //yurpusCommandText.text = currentObject;
-
-        gameObject.SetActive(false);
     }
 }
